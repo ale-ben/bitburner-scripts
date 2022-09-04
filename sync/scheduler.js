dfabcefb053e0cdcac8c4c0ce6809fb2a66bd12e
Alessandro Benetton <aleben98@gmail.com>
2022-09-04 21:43:08 +0200

    implement status boolean

--- sync/scheduler.js
+++ sync/scheduler.js
@@ -7,7 +7,7 @@ async function manageNodes(ns) {
 }
 
 async function harvest(ns){
-	ns.run("/bin/centralizedLaunchHarvest.js", 1,  "n00dles");
+	ns.run("/bin/centralizedLaunchHarvest.js", 1,  "foodnstuff");
 }
 
 export async function main(ns) {
@@ -22,16 +22,19 @@ export async function main(ns) {
 		{
 			name: "attack",
 			func: attack,
+			status: true,
 			interval: 10 // in minutes
 		},
 		{
 			name: "manageNodes",
 			func: manageNodes,
+			status: true,
 			interval: 5 // in minutes
 		},
 		{
 			name: "harvest",
 			func: harvest,
+			status: false,
 			interval: 10 // in minutes
 		}
 	]
@@ -41,7 +44,7 @@ export async function main(ns) {
 	while (true) {
 
 		for (const func of functionList) {
-			if (!func.hasOwnProperty('lastRun') || (counter-func.lastRun>func.interval)){
+			if (func.status && (!func.hasOwnProperty('lastRun') || (counter-func.lastRun>func.interval))){
 				ns.print("Running " + func.name);
 				if (counter>=1000*60*60*24) func.lastRun = 0;
 				else func.lastRun = counter;
