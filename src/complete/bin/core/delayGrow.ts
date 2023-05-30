c0a17eef44dffc622ba378843a874b7ec7a4eb82
Alessandro Benetton <aleben98@gmail.com>
2023-05-30 19:03:00 +0200

    Implemented core delay scripts

---
+++ src/complete/bin/core/delayGrow.ts
@@ -0,0 +1,20 @@
+import {NS} from "@ns";
+
+export async function main(ns : NS): Promise<void> {
+	const hostname = ns.args[0];
+	if (!hostname) {
+		throw new Error("No hostname specified.");
+	}
+	if (typeof hostname !== "string") {
+		throw new Error("Hostname must be a string.");
+	}
+	const delayMS = ns.args[1];
+	if (!delayMS) {
+		throw new Error("No delay specified.");
+	}
+	if (typeof delayMS !== "number") {
+		throw new Error("Delay must be a number.");
+	}
+	await ns.sleep(delayMS);
+	await ns.grow(hostname);
+}
