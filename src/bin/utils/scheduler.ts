2d7a80562e295c336789b5cedda906bb70b49fc4
Alessandro Benetton <aleben98@gmail.com>
2024-05-05 21:17:11 +0200

    Autoreload scheduler

--- src/bin/utils/scheduler.ts
+++ src/bin/utils/scheduler.ts
@@ -14,16 +14,24 @@ export async function main(ns: NS) {
 	ns.disableLog('exec');
 	ns.disableLog('scp');
 
-	const scheduleStr = ns.read('/config/schedulerData.txt');
-	const schedule: scheduleElement[] = parseScheduleString(ns, scheduleStr);
+	let schedule: scheduleElement[] = loadSchedule(ns);
 
 	let counter = 0; // Passed minutes since start (or rollover)
 
 	while (true) {
+
+		if (counter % 5 == 0) {
+			// Every 5 minutes, reload the schedule
+			ns.print("DEBUG: Reloading schedule");
+			const newSched = loadSchedule(ns);
+			schedule = mergeSchedules(schedule, newSched);
+			ns.print("INFO: Schedule updated")
+		}
+
 		for (const el of schedule) {
 			if (el.status && (!el.lastRun || counter - el.lastRun > el.interval || counter < el.lastRun)) {
 				// Should run the program
-				ns.print('Running' + el.name);
+				ns.print('INFO: Running ' + el.name);
 
 				// Rollover for counter > 24 hours
 				if (counter >= 60 * 24) el.lastRun = 0;
@@ -117,4 +125,45 @@ function parseScheduleString(ns: NS, scheduleStr: string) {
 	}
 
 	return returnArr;
+}
+
+/**
+ * Merges multiple schedules preserving lastRun from baseEl
+ * @param baseEl 
+ * @param newEl 
+ * @returns 
+ */
+function mergeSchedules(baseEl: scheduleElement[], newEl: scheduleElement[]): scheduleElement[] {
+	const returnEl = baseEl;
+
+	for (const el of newEl) {
+		const base = returnEl.findIndex(elem => {
+			elem.name === el.name
+		});
+
+		if (base === -1) {
+			// Can't find any element with the same name. push new element
+			returnEl.push(el);
+			continue;
+		}
+
+		// Found a correspondence, overwrite it but keep lastRun
+		const lastRun = returnEl[base].lastRun;
+		returnEl[base] = {
+			lastRun,
+			...el
+		};
+	}
+
+	return returnEl;
+}
+
+/**
+ * Load a schedule from file
+ * @param ns 
+ * @returns 
+ */
+function loadSchedule(ns: NS): scheduleElement[] {
+	const scheduleStr = ns.read('/config/schedulerData.txt');
+	return parseScheduleString(ns, scheduleStr);
 }
